5ff6ba1110450b8e7679dd6566f44838176516a5
Paulo Pereira <paulo.jorge.pereira@gmail.com>
2018-03-28 16:36:50 +0100

    Update mysqlConfig.js

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection({
-   host: "10.21.8.242",
+   host: "10.21.8.144",
    user: "root",
    password: "nutanix/4u",
    database: "uptick"
